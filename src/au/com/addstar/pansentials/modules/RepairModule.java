d83c03d30ff1cc7e1fe35fce2ce27288dea54056
addstar <addstar@addstar.com.au>
2014-10-11 18:21:23 +1100

    (RepairModule) Actually make it work + Check for items that should be repaired + Add /repair all

--- src/au/com/addstar/pansentials/modules/RepairModule.java
+++ src/au/com/addstar/pansentials/modules/RepairModule.java
@@ -1,13 +1,16 @@
 package au.com.addstar.pansentials.modules;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import au.com.addstar.pansentials.MasterPlugin;
 import au.com.addstar.pansentials.Module;
+import au.com.addstar.pansentials.Utilities;
 
 public class RepairModule implements Module, CommandExecutor{
 	
@@ -33,13 +36,42 @@ public class RepairModule implements Module, CommandExecutor{
 			String[] args) {
 		if(command.getName().equalsIgnoreCase("repair") && sender instanceof Player){
 			Player ply = (Player) sender;
-			ItemStack stack = ply.getItemInHand();
-			if(stack != null){
-				stack.setDurability(stack.getType().getMaxDurability());
+			if (args.length == 0) {
+				ItemStack stack = ply.getItemInHand();
+				if (stack == null || stack.getType().isBlock() || stack.getDurability() == 0)
+					return true;
+				
+				stack.setDurability((short) 0);
+				ply.updateInventory();
+				sender.sendMessage(Utilities.format(plugin.getFormatConfig(), "repair.hand"));
+			} else {
+				if (args[0].equalsIgnoreCase("all")) {
+					for (ItemStack stack : ply.getInventory().getContents()) {
+						if (needsRepair(stack))
+							stack.setDurability((short) 0);
+					}
+					for (ItemStack stack : ply.getInventory().getArmorContents()) {
+						if (needsRepair(stack))
+							stack.setDurability((short) 0);
+					}
+				}
+				sender.sendMessage(Utilities.format(plugin.getFormatConfig(), "repair.all"));
 			}
-			ply.updateInventory();
 		}
 		return true;
 	}
+	
+	private boolean needsRepair(ItemStack stack) {
+		if (stack == null)
+			return false;
 
+		Material mat = stack.getType();
+		if (stack.getDurability() == 0 || stack.getMaxStackSize() < 0 ||
+				mat.isBlock() || mat.isRecord() || 
+				mat == Material.MONSTER_EGG || mat == Material.MONSTER_EGGS ||
+				mat == Material.SKULL || mat == Material.SKULL_ITEM)
+					return false;
+
+		return true;
+	}
 }
