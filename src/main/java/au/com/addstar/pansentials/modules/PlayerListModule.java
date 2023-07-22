4c47a27761b738a6f8cf1e0312211dcd89f66a69
addstar <addstar@addstar.com.au>
2023-07-23 00:42:19 +1000

    Remove /list alias because CCR breaks the command if we use it

--- src/main/java/au/com/addstar/pansentials/modules/PlayerListModule.java
+++ src/main/java/au/com/addstar/pansentials/modules/PlayerListModule.java
@@ -70,65 +70,63 @@ public class PlayerListModule implements Module, CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String cmd,
 							 String[] args) {
-		if (command.getName().equalsIgnoreCase("mvw")) {
-			// Get all players online
-			Collection<? extends Player> players = Bukkit.getOnlinePlayers();
+		// Get all players online
+		Collection<? extends Player> players = Bukkit.getOnlinePlayers();
 
-			// Create a map to hold worlds and their players
-			Map<World, List<PlayerInfo>> worldPlayersMap = new HashMap<>();
+		// Create a map to hold worlds and their players
+		Map<World, List<PlayerInfo>> worldPlayersMap = new HashMap<>();
 
-			// Iterate over all online players
-			for (Player player : players) {
-				// Check if player is hidden from the command invoker
-				boolean vanished = isVanished(player);
-				if (!vanished || (vanished && canSeeVanished(sender))) {
-					// Get the player's display name
-					PlayerInfo pinfo = new PlayerInfo(player.getDisplayName(), chat.getPlayerSuffix(player));
+		// Iterate over all online players
+		for (Player player : players) {
+			// Check if player is hidden from the command invoker
+			boolean vanished = isVanished(player);
+			if (!vanished || (vanished && canSeeVanished(sender))) {
+				// Get the player's display name
+				PlayerInfo pinfo = new PlayerInfo(player.getDisplayName(), chat.getPlayerSuffix(player));
 
-					// Add the player to their world's list in the map
-					worldPlayersMap.computeIfAbsent(player.getWorld(), k -> new ArrayList<>()).add(pinfo);
-				}
+				// Add the player to their world's list in the map
+				worldPlayersMap.computeIfAbsent(player.getWorld(), k -> new ArrayList<>()).add(pinfo);
+			}
+		}
+
+		// Output total number of players on the server
+		sender.sendMessage(ChatColor.AQUA + "--- Worlds and players (" + players.size() + "): ---");
+
+		// Convert the map's entry set to a list
+		List<Map.Entry<World, List<PlayerInfo>>> worldEntries = new ArrayList<>(worldPlayersMap.entrySet());
+
+		// Sort the list by world name
+		worldEntries.sort(Comparator.comparing(e -> e.getKey().getName(), String.CASE_INSENSITIVE_ORDER));
+
+		// Iterate over each entry in the sorted list
+		for (Map.Entry<World, List<PlayerInfo>> entry : worldEntries) {
+			// Get the world and list of player display names
+			World world = entry.getKey();
+			List<PlayerInfo> playerInfos = entry.getValue();
+
+			// Sort the list of player display names alphabetically
+			playerInfos.sort(Comparator.comparing(pi -> pi.getDisplayName(), String.CASE_INSENSITIVE_ORDER));
+
+			// Use a StringBuilder to build the line
+			StringBuilder line = new StringBuilder(ChatColor.GREEN + world.getName() + " "
+					+ ChatColor.YELLOW + "(" + playerInfos.size() + ")"
+					+ ChatColor.WHITE + ": ");
+
+			// Add each player name to the line
+			for (PlayerInfo playerInfo : playerInfos) {
+				line.append(playerInfo.getPlayerColour())
+						.append(playerInfo.displayName)
+						.append(ChatColor.WHITE)
+						.append(", ");
 			}
 
-			// Output total number of players on the server
-			sender.sendMessage(ChatColor.AQUA + "--- Worlds and players (" + players.size() + "): ---");
-
-			// Convert the map's entry set to a list
-			List<Map.Entry<World, List<PlayerInfo>>> worldEntries = new ArrayList<>(worldPlayersMap.entrySet());
-
-			// Sort the list by world name
-			worldEntries.sort(Comparator.comparing(e -> e.getKey().getName(), String.CASE_INSENSITIVE_ORDER));
-
-			// Iterate over each entry in the sorted list
-			for (Map.Entry<World, List<PlayerInfo>> entry : worldEntries) {
-				// Get the world and list of player display names
-				World world = entry.getKey();
-				List<PlayerInfo> playerInfos = entry.getValue();
-
-				// Sort the list of player display names alphabetically
-				playerInfos.sort(Comparator.comparing(pi -> pi.getDisplayName(), String.CASE_INSENSITIVE_ORDER));
-
-				// Use a StringBuilder to build the line
-				StringBuilder line = new StringBuilder(ChatColor.GREEN + world.getName() + " "
-						+ ChatColor.YELLOW + "(" + playerInfos.size() + ")"
-						+ ChatColor.WHITE + ": ");
-
-				// Add each player name to the line
-				for (PlayerInfo playerInfo : playerInfos) {
-					line.append(playerInfo.getPlayerColour())
-							.append(playerInfo.displayName)
-							.append(ChatColor.WHITE)
-							.append(", ");
-				}
-
-				// Remove the trailing comma and space
-				if (line.length() > 0) {
-					line.setLength(line.length() - 4);  // Chopping off trailing comma, space and colour bytes
-				}
-
-				// Output the line
-				sender.sendMessage(line.toString());
+			// Remove the trailing comma and space
+			if (line.length() > 0) {
+				line.setLength(line.length() - 4);  // Chopping off trailing comma, space and colour bytes
 			}
+
+			// Output the line
+			sender.sendMessage(line.toString());
 		}
 		return true;
 	}
